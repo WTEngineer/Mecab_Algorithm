14d91dcc2a66514cf271d4356ee5e16eb323bd6f
Software Developer <133099486+hereistopdev@users.noreply.github.com>
2025-02-15 00:51:26 +0800

    Add ipadic dic and test script

---
+++ dictest/ipadic.py
@@ -0,0 +1,21 @@
+import MeCab
+import ipadic
+import os
+import sys
+
+def get_version(dicdir):
+    vpath = os.path.join(dicdir, 'version')
+    with open(vpath) as vfile:
+        return vfile.read().strip()
+
+_curdir = os.path.dirname(__file__)
+
+# This will be used elsewhere to initialize the tagger
+DICDIR = os.path.join(_curdir, 'dicdir')
+VERSION = get_version(DICDIR)
+
+mecabrc = os.path.join(DICDIR, 'mecabrc')
+MECAB_ARGS = '-r "{}" -d "{}"'.format(mecabrc, DICDIR)
+
+tagger = MeCab.Tagger(MECAB_ARGS)
+print(tagger.parse("図書館にいた事がバレた"))
