360486c056a03e06146acdd3df59f6badac5ed10
Software Developer <133099486+hereistopdev@users.noreply.github.com>
2025-02-18 20:50:39 +0800

    Change dic path and add test scripts

--- dic/ipadic.py
+++ dic/ipadic.py
@@ -1,18 +1,11 @@
 import MeCab
-import ipadic
 import os
 import sys
-
-def get_version(dicdir):
-    vpath = os.path.join(dicdir, 'version')
-    with open(vpath) as vfile:
-        return vfile.read().strip()
 
 _curdir = os.path.dirname(__file__)
 
 # This will be used elsewhere to initialize the tagger
-DICDIR = os.path.join(_curdir, 'dicdir')
-VERSION = get_version(DICDIR)
+DICDIR = os.path.join(_curdir, 'ipadicdicdir')
 
 mecabrc = os.path.join(DICDIR, 'mecabrc')
 MECAB_ARGS = '-r "{}" -d "{}"'.format(mecabrc, DICDIR)
